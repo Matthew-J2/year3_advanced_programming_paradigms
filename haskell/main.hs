800caf1e7dcfbd8d96b62b454c226ef05c92a56f
Matthew James <jamesm21@coventry.ac.uk>
2025-02-18 02:28:48 +0000

    File is read in and placeholder scan function added

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,7 +1,14 @@
 module Main where
 main :: IO ()
-main = return ()
+main = do
+  source <- readFile "source.txt"
+  putStrLn source
+
 data Tokens = 
   HEADER1 | HEADER2 | HEADER3 | PARAGRAPH | BOLD | OLIST | ULIST | LISTITEM | WHITESPACE | EOF
   deriving(Show, Eq)
- 
+  
+scan x 
+  | x == "0" = HEADER1
+  | x == "1" = HEADER2
+  | x == "2" = HEADER2
