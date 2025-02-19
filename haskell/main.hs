edb57a742a676362d7c42e1c9f4e1d32192432e9
Matthew James <jamesm21@coventry.ac.uk>
2025-02-19 12:49:52 +0000

    Removed leading whitespace and added cases for other headings, added otherwise case for paragraphs

--- haskell/main.hs
+++ haskell/main.hs
@@ -2,7 +2,9 @@ module Main where
 main :: IO ()
 main = do
   source <- readFile "source.txt"
-  putStrLn source
+  --putStrLn source
+  let scanned = scan(source)
+  putStrLn (show scanned)
   writeFile "output.html" source
 
 data Token = 
@@ -10,17 +12,28 @@ data Token =
   | OLIST String | ULIST String | LISTITEM String | WHITESPACE String | EOF
   deriving(Show, Eq)
 
+rm_wspace :: String -> String
+rm_wspace [] = []
+rm_wspace (srch : srct)
+  | srch == ' ' || srch == '\n' = rm_wspace(srct)
+  | otherwise = srch : srct
+
 scan :: String -> [Token] -- define type signature
 scan [] = [] -- handles empty case
-
 scan source 
   --if first 2 characters match, let the head and rest of the file = the current line minus the first 2 characters, 
   --and the rest of the file be the rest
   --create a token with the head of the text then call scan with the rest of the file
-  --TODO: get rid of leading whitespace and write cases for other tokens
+  --TODO: write cases for bold and numbered/un-numbered lists
   | take 2 source == "0 " = 
       let (headTxt, rest) = break (== '\n') (drop 2 source)
-      in HEADER1 headTxt : scan(rest)
-  | source == "1 " = [HEADER2 "<h2> something </h2>"]
-  | source == "2 " = [HEADER3 "<h3> something </h3>"]
+      in HEADER1 headTxt : scan(rm_wspace (rest))
+  | take 2 source == "1 " = 
+      let (headTxt, rest) = break (== '\n') (drop 2 source)
+      in HEADER2 headTxt : scan(rm_wspace(rest))
+  | take 2 source == "2 " = 
+      let (headTxt, rest) = break (== '\n') (drop 2 source)
+      in HEADER3 headTxt : scan(rm_wspace (rest))
+  | otherwise = let (headTxt, rest) = break (== '\n') source
+      in PARAGRAPH headTxt : scan(rm_wspace (rest))
 
