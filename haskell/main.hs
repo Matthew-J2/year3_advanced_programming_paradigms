d87766fc18f426c05ebc7c41a82018b41a1d4245
Matthew James <jamesm21@coventry.ac.uk>
2025-02-20 01:51:56 +0000

    Wrote cases for bold text and changed otherwise to check for bold text tokens

--- haskell/main.hs
+++ haskell/main.hs
@@ -8,7 +8,7 @@ main = do
   writeFile "output.html" source
 
 data Token = 
-  HEADER1 String | HEADER2 String | HEADER3 String | PARAGRAPH String | BOLD String 
+  HEADER1 String | HEADER2 String | HEADER3 String | PARAGRAPH String | BOLDL String | BOLDR String
   | OLIST String | ULIST String | LISTITEM String | WHITESPACE String | EOF
   deriving(Show, Eq)
 
@@ -19,12 +19,13 @@ rm_wspace (srch : srct)
   | otherwise = srch : srct
 
 scan :: String -> [Token] -- define type signature
-scan [] = [] -- handles empty case
+scan [] = [EOF] -- handles empty case
 scan source 
   --if first 2 characters match, let the head and rest of the file = the current line minus the first 2 characters, 
   --and the rest of the file be the rest
   --create a token with the head of the text then call scan with the rest of the file
-  --TODO: write cases for bold and numbered/un-numbered lists
+  --TODO: write cases for numbered/un-numbered lists
+  --TODO: change scanner to split on tokens instead of just \n
   | take 2 source == "0 " = 
       let (headTxt, rest) = break (== '\n') (drop 2 source)
       in HEADER1 headTxt : scan(rm_wspace (rest))
@@ -34,6 +35,10 @@ scan source
   | take 2 source == "2 " = 
       let (headTxt, rest) = break (== '\n') (drop 2 source)
       in HEADER3 headTxt : scan(rm_wspace (rest))
-  | otherwise = let (headTxt, rest) = break (== '\n') source
+  | take 1 source == "[" = 
+      BOLDL "[" : scan(rm_wspace (drop 1 source))
+  | take 1 source == "]" = 
+      BOLDR "]" : scan(rm_wspace (drop 1 source))
+  | otherwise = let (headTxt, rest) = break (\c -> c == '\n' || c == '[' || c == ']') source
       in PARAGRAPH headTxt : scan(rm_wspace (rest))
 
