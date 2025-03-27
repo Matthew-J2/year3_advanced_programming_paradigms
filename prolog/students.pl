e7342fc5c86888b3fc3209cfaae5dce9f7e2382f
Matthew James <jamesm21@coventry.ac.uk>
2025-03-27 02:58:36 +0000

    Added another student and fixed web_dev issue

--- prolog/students.pl
+++ prolog/students.pl
@@ -1,5 +1,5 @@
 grade(john, introduction_to_programming, 65).
-grade(john, introduction_to_computer_science, 73).
+grade(john, introduction_to_computer_science, 74).
 
 grade(greg, introduction_to_programming, 54).
 grade(greg, introduction_to_computer_science, 43).
@@ -9,6 +9,15 @@ grade(unlucky_bob, introduction_to_computer_science, 31).
 
 grade(ai_lover_steve, introduction_to_programming, 53).
 grade(ai_lover_steve, introduction_to_computer_science, 53).
+
+grade(simon, introduction_to_programming, 40).
+grade(simon, introduction_to_computer_science, 40).
+grade(simon, web_development, 40).
+grade(simon, intro_to_data_structures_algorithms, 40).
+grade(simon, maths_for_computer_science, 40).
+grade(simon, computer_architecture, 40).
+grade(simon, software_engineering, 40).
+grade(simon, operating_systems, 40).
 
 grade(embedded_emily, introduction_to_programming, 87).
 grade(embedded_emily, introduction_to_computer_science, 95).
@@ -37,9 +46,9 @@ grade(academic_amy, computer_architecture, 89).
 grade(academic_amy, software_engineering, 91).
 grade(academic_amy, operating_systems, 86).
 
-interest(john, webdev).
-interest(greg, webdev).
-interest(unlucky_bob, webdev).
+interest(john, web_dev).
+interest(greg, web_dev).
+interest(unlucky_bob, web_dev).
 interest(ai_lover_steve, ai).
 interest(embedded_emily, embedded).
 interest(cybersecurity_sally, cybersecurity).
